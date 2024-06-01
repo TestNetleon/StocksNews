6e1239f822a2fb1a08ee2bea3266e33b8ccf8b08
Netleon Projects <46236590+TestNetleon@users.noreply.github.com>
2024-06-01 19:38:51 +0530

    change

--- lib/screens/stocks/container.dart
+++ lib/screens/stocks/container.dart
@@ -15,7 +15,6 @@ import 'package:stocks_news_new/utils/theme.dart';
 import 'package:stocks_news_new/utils/utils.dart';
 import 'package:stocks_news_new/widgets/base_container.dart';
 import 'package:stocks_news_new/widgets/base_ui_container.dart';
-import 'package:stocks_news_new/widgets/loading.dart';
 import 'package:stocks_news_new/widgets/refresh_controll.dart';
 import 'package:stocks_news_new/widgets/screen_title.dart';
 import 'package:stocks_news_new/widgets/spacer_horizontal.dart';
@@ -75,86 +74,84 @@ class StocksContainer extends StatelessWidget {
               searching: provider.isSearching,
               editable: true,
             ),
-            provider.isLoading
-                ? const Loading()
-                : Expanded(
-                    child: BaseUiContainer(
-                      isLoading: provider.isLoading,
-                      hasData: provider.data != null &&
-                          (provider.data?.isNotEmpty ?? false) &&
-                          !provider.isLoading,
-                      error: provider.error,
-                      child: RefreshControl(
-                        onRefresh: () async =>
-                            provider.getData(showProgress: false),
-                        canLoadMore: provider.canLoadMore,
-                        onLoadMore: () async =>
-                            provider.getData(loadMore: true, clear: false),
-                        child: ListView.separated(
-                          itemCount: provider.data?.length ?? 0,
-                          padding: EdgeInsets.symmetric(vertical: 10.sp),
-                          separatorBuilder: (context, index) {
-                            // return const SpacerVertical(height: 10);
-                            return Divider(
+            Expanded(
+              child: BaseUiContainer(
+                isLoading: provider.isLoading,
+                hasData: provider.data != null &&
+                    (provider.data?.isNotEmpty ?? false) &&
+                    !provider.isLoading,
+                error: provider.error,
+                showPreparingText: true,
+                child: RefreshControl(
+                  onRefresh: () async => provider.getData(showProgress: false),
+                  canLoadMore: provider.canLoadMore,
+                  onLoadMore: () async =>
+                      provider.getData(loadMore: true, clear: false),
+                  child: ListView.separated(
+                    itemCount: provider.data?.length ?? 0,
+                    padding: EdgeInsets.symmetric(vertical: 10.sp),
+                    separatorBuilder: (context, index) {
+                      // return const SpacerVertical(height: 10);
+                      return Divider(
+                        color: ThemeColors.greyBorder,
+                        height: 12.sp,
+                      );
+                    },
+                    itemBuilder: (context, index) {
+                      AllStocks? data = provider.data?[index];
+                      if (index == 0) {
+                        return Column(
+                          children: [
+                            Divider(
                               color: ThemeColors.greyBorder,
-                              height: 12.sp,
-                            );
-                          },
-                          itemBuilder: (context, index) {
-                            AllStocks? data = provider.data?[index];
-                            if (index == 0) {
-                              return Column(
-                                children: [
-                                  Divider(
-                                    color: ThemeColors.greyBorder,
-                                    height: 15.sp,
-                                    thickness: 1,
+                              height: 15.sp,
+                              thickness: 1,
+                            ),
+                            Row(
+                              children: [
+                                const SpacerHorizontal(width: 5),
+                                Expanded(
+                                  child: AutoSizeText(
+                                    maxLines: 1,
+                                    "COMPANY",
+                                    style: stylePTSansRegular(
+                                      fontSize: 12,
+                                      color: ThemeColors.greyText,
+                                    ),
                                   ),
-                                  Row(
-                                    children: [
-                                      const SpacerHorizontal(width: 5),
-                                      Expanded(
-                                        child: AutoSizeText(
-                                          maxLines: 1,
-                                          "COMPANY",
-                                          style: stylePTSansRegular(
-                                            fontSize: 12,
-                                            color: ThemeColors.greyText,
-                                          ),
-                                        ),
-                                      ),
-                                      const SpacerHorizontal(width: 24),
-                                      Expanded(
-                                        child: AutoSizeText(
-                                          maxLines: 1,
-                                          "PRICE",
-                                          style: stylePTSansRegular(
-                                            fontSize: 12,
-                                            color: ThemeColors.greyText,
-                                          ),
-                                        ),
-                                      ),
-                                      const SpacerHorizontal(width: 10),
-                                    ],
+                                ),
+                                const SpacerHorizontal(width: 24),
+                                Expanded(
+                                  child: AutoSizeText(
+                                    maxLines: 1,
+                                    "PRICE",
+                                    style: stylePTSansRegular(
+                                      fontSize: 12,
+                                      color: ThemeColors.greyText,
+                                    ),
                                   ),
-                                  Divider(
-                                    color: ThemeColors.greyBorder,
-                                    height: 15.sp,
-                                    thickness: 1,
-                                  ),
-                                  StocksItemAll(data: data, index: index),
-                                ],
-                              );
-                            }
-                            return StocksItemAll(
-                              index: index,
-                              data: data,
-                            );
-                          },
-                        ),
-                      ),
-                    ),
+                                ),
+                                const SpacerHorizontal(width: 10),
+                              ],
+                            ),
+                            Divider(
+                              color: ThemeColors.greyBorder,
+                              height: 15.sp,
+                              thickness: 1,
+                            ),
+                            StocksItemAll(data: data, index: index),
+                          ],
+                        );
+                      }
+                      return StocksItemAll(
+                        index: index,
+                        data: data,
+                      );
+                    },
                   ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
