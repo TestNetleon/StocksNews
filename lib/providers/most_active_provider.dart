86692d6b54e619be2875efd0db675a1f1f09abe1
Nirmal <testnetleon@gmail.com>
2024-06-08 11:10:14 +0530

    most active ui

--- lib/providers/most_active_provider.dart
+++ lib/providers/most_active_provider.dart
@@ -1,60 +1,197 @@
+// import 'dart:convert';
+
+// import 'package:flutter/foundation.dart';
+// import 'package:stocks_news_new/api/api_requester.dart';
+// import 'package:stocks_news_new/api/api_response.dart';
+// import 'package:stocks_news_new/api/apis.dart';
+// import 'package:stocks_news_new/modals/most_active_stocks_res.dart';
+// import 'package:stocks_news_new/providers/auth_provider_base.dart';
+// import 'package:stocks_news_new/utils/constants.dart';
+// import 'package:stocks_news_new/utils/utils.dart';
+
+// class MostActiveProvider extends ChangeNotifier with AuthProviderBase {
+//   Status _status = Status.ideal;
+//   // ************* GAP DOWN **************** //
+//   String? _error;
+//   int _page = 1;
+//   Extra? _extraUp;
+//   List<MostActiveStocksRes>? _data;
+
+//   List<MostActiveStocksRes>? get data => _data;
+//   List<MostActiveStocksRes>? _dataMostVolatile;
+
+//   List<MostActiveStocksRes>? get dataMostVolatile => _dataMostVolatile;
+//   List<MostActiveStocksRes>? _dataUnusualTradingVolume;
+
+//   List<MostActiveStocksRes>? get dataUnusualTradingVolume =>
+//       _dataUnusualTradingVolume;
+//   Extra? get extraUp => _extraUp;
+//   bool get canLoadMore => _page < (_extraUp?.totalPages ?? 1);
+//   String? get error => _error ?? Const.errSomethingWrong;
+//   bool get isLoading => _status == Status.loading;
+
+//   // ************* GAP DOWN **************** //
+//   String? _errorDown;
+//   int _pageDown = 1;
+//   // int? _totalPageDown;
+//   Extra? _extraDown;
+
+//   bool get canLoadMoreDown => _pageDown < (_extraDown?.totalPages ?? 1);
+//   String? get errorDown => _errorDown ?? Const.errSomethingWrong;
+//   bool get isLoadingDown => _status == Status.loading;
+//   Status _statusMostActive = Status.ideal;
+//   Status get statusMostActive => _statusMostActive;
+//   bool get isLoadingMostActive => _statusMostActive == Status.loading;
+//   int get openIndexMostActive => _openIndexMostActive;
+//   int _openIndexMostActive = -1;
+//   int _openIndex = -1;
+
+//   int get openIndex => _openIndex;
+
+//   void setStatus(status) {
+//     _status = status;
+//     notifyListeners();
+//   }
+
+//   void setStatusMostActive(status) {
+//     _statusMostActive = status;
+//     notifyListeners();
+//   }
+
+//   void setOpenIndex(index) {
+//     _openIndex = index;
+//     notifyListeners();
+//   }
+
+//   void setOpenIndexMostActive(index) {
+//     _openIndexMostActive = index;
+//     notifyListeners();
+//   }
+
+//   Future getMostActiveData({loadMore = false, type = 1}) async {
+//     if (loadMore) {
+//       _page++;
+//       setStatus(Status.loadingMore);
+//     } else {
+//       _page = 1;
+//       setStatus(Status.loading);
+//     }
+//     try {
+//       Map request = {"page": "$_page"};
+
+//       ApiResponse response = await apiRequest(
+//         url: type == 1
+//             ? Apis.mostActiveStocks
+//             : type == 2
+//                 ? Apis.mostVoliatileStocks
+//                 : Apis.mostVolumeStocks,
+//         request: request,
+//         showProgress: false,
+//       );
+
+//       if (response.status) {
+//         _error = null;
+//         if (_page == 1) {
+//           type == 1
+//               ? _data = mostActiveStocksFromJson(jsonEncode(response.data))
+//               : type == 2
+//                   ? _dataMostVolatile =
+//                       mostActiveStocksFromJson(jsonEncode(response.data))
+//                   : _dataUnusualTradingVolume =
+//                       mostActiveStocksFromJson(jsonEncode(response.data));
+
+//           _extraUp = response.extra is Extra ? response.extra : null;
+//         } else {
+//           type == 1
+//               ? _data
+//                   ?.addAll(mostActiveStocksFromJson(jsonEncode(response.data)))
+//               : type == 2
+//                   ? _dataMostVolatile?.addAll(
+//                       mostActiveStocksFromJson(jsonEncode(response.data)))
+//                   : _dataUnusualTradingVolume?.addAll(
+//                       mostActiveStocksFromJson(jsonEncode(response.data)));
+//         }
+//       } else {
+//         if (_page == 1) {
+//           type == 1
+//               ? _data = null
+//               : type == 2
+//                   ? _dataMostVolatile = null
+//                   : _dataUnusualTradingVolume = null;
+//           _error = response.message;
+//           // showErrorMessage(message: response.message);
+//         }
+//       }
+//       setStatus(Status.loaded);
+//     } catch (e) {
+//       type == 1
+//           ? _data = null
+//           : type == 2
+//               ? _dataMostVolatile = null
+//               : _dataUnusualTradingVolume = null;
+//       Utils().showLog(e.toString());
+//       setStatus(Status.loaded);
+//     }
+//   }
+// }
+
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
 import 'package:stocks_news_new/api/api_requester.dart';
 import 'package:stocks_news_new/api/api_response.dart';
 import 'package:stocks_news_new/api/apis.dart';
 import 'package:stocks_news_new/modals/most_active_stocks_res.dart';
 import 'package:stocks_news_new/providers/auth_provider_base.dart';
+import 'package:stocks_news_new/providers/filter_provider.dart';
+import 'package:stocks_news_new/providers/user_provider.dart';
+import 'package:stocks_news_new/route/my_app.dart';
 import 'package:stocks_news_new/utils/constants.dart';
 import 'package:stocks_news_new/utils/utils.dart';
 
 class MostActiveProvider extends ChangeNotifier with AuthProviderBase {
   Status _status = Status.ideal;
-  // ************* GAP DOWN **************** //
+
   String? _error;
   int _page = 1;
   Extra? _extraUp;
   List<MostActiveStocksRes>? _data;
 
   List<MostActiveStocksRes>? get data => _data;
-  List<MostActiveStocksRes>? _dataMostVolatile;
 
-  List<MostActiveStocksRes>? get dataMostVolatile => _dataMostVolatile;
-  List<MostActiveStocksRes>? _dataUnusualTradingVolume;
-
-  List<MostActiveStocksRes>? get dataUnusualTradingVolume =>
-      _dataUnusualTradingVolume;
   Extra? get extraUp => _extraUp;
   bool get canLoadMore => _page < (_extraUp?.totalPages ?? 1);
   String? get error => _error ?? Const.errSomethingWrong;
   bool get isLoading => _status == Status.loading;
 
-  // ************* GAP DOWN **************** //
-  String? _errorDown;
-  int _pageDown = 1;
-  // int? _totalPageDown;
-  Extra? _extraDown;
-
-  bool get canLoadMoreDown => _pageDown < (_extraDown?.totalPages ?? 1);
-  String? get errorDown => _errorDown ?? Const.errSomethingWrong;
-  bool get isLoadingDown => _status == Status.loading;
-  Status _statusMostActive = Status.ideal;
-  Status get statusMostActive => _statusMostActive;
-  bool get isLoadingMostActive => _statusMostActive == Status.loading;
-  int get openIndexMostActive => _openIndexMostActive;
-  int _openIndexMostActive = -1;
-  int _openIndex = -1;
-
   int get openIndex => _openIndex;
+  int _openIndex = -1;
+  FilteredParams? _filterParams;
+  FilteredParams? get filterParams => _filterParams;
 
-  void setStatus(status) {
-    _status = status;
+  void resetFilter() {
+    _filterParams = null;
+    _page = 1;
     notifyListeners();
   }
 
-  void setStatusMostActive(status) {
-    _statusMostActive = status;
+  void applyFilter(FilteredParams? params) {
+    _filterParams = params;
+    _page = 1;
+    notifyListeners();
+    getData();
+  }
+
+  void exchangeFilter(String item) {
+    _filterParams!.exchange_name!.remove(item);
+    _page = 1;
+    notifyListeners();
+    getData();
+  }
+
+  void setStatus(status) {
+    _status = status;
     notifyListeners();
   }
 
@@ -63,12 +200,11 @@ class MostActiveProvider extends ChangeNotifier with AuthProviderBase {
     notifyListeners();
   }
 
-  void setOpenIndexMostActive(index) {
-    _openIndexMostActive = index;
-    notifyListeners();
+  Future onRefresh() async {
+    // getHighLowNegativeBetaStocks();
   }
 
-  Future getMostActiveData({loadMore = false, type = 1}) async {
+  Future getData({loadMore = false, type = 1}) async {
     if (loadMore) {
       _page++;
       setStatus(Status.loadingMore);
@@ -77,58 +213,40 @@ class MostActiveProvider extends ChangeNotifier with AuthProviderBase {
       setStatus(Status.loading);
     }
     try {
-      Map request = {"page": "$_page"};
+      Map request = {
+        "token":
+            navigatorKey.currentContext!.read<UserProvider>().user?.token ?? "",
+        "page": "$_page",
+        "exchange_name": _filterParams?.exchange_name?.join(",") ?? "",
+      };
 
       ApiResponse response = await apiRequest(
-        url: type == 1
-            ? Apis.mostActiveStocks
-            : type == 2
-                ? Apis.mostVoliatileStocks
-                : Apis.mostVolumeStocks,
+        url: Apis.mostActiveStocks,
         request: request,
         showProgress: false,
+        onRefresh: onRefresh,
       );
 
       if (response.status) {
         _error = null;
         if (_page == 1) {
-          type == 1
-              ? _data = mostActiveStocksFromJson(jsonEncode(response.data))
-              : type == 2
-                  ? _dataMostVolatile =
-                      mostActiveStocksFromJson(jsonEncode(response.data))
-                  : _dataUnusualTradingVolume =
-                      mostActiveStocksFromJson(jsonEncode(response.data));
+          _data = mostActiveStocksFromJson(jsonEncode(response.data));
 
           _extraUp = response.extra is Extra ? response.extra : null;
         } else {
-          type == 1
-              ? _data
-                  ?.addAll(mostActiveStocksFromJson(jsonEncode(response.data)))
-              : type == 2
-                  ? _dataMostVolatile?.addAll(
-                      mostActiveStocksFromJson(jsonEncode(response.data)))
-                  : _dataUnusualTradingVolume?.addAll(
-                      mostActiveStocksFromJson(jsonEncode(response.data)));
+          _data?.addAll(mostActiveStocksFromJson(jsonEncode(response.data)));
         }
       } else {
         if (_page == 1) {
-          type == 1
-              ? _data = null
-              : type == 2
-                  ? _dataMostVolatile = null
-                  : _dataUnusualTradingVolume = null;
+          _data = null;
+          _data = null;
           _error = response.message;
           // showErrorMessage(message: response.message);
         }
       }
       setStatus(Status.loaded);
     } catch (e) {
-      type == 1
-          ? _data = null
-          : type == 2
-              ? _dataMostVolatile = null
-              : _dataUnusualTradingVolume = null;
+      _data = null;
       Utils().showLog(e.toString());
       setStatus(Status.loaded);
     }
--- lib/screens/drawerScreens/mostActive/most_active_stocks.dart
+++ lib/screens/drawerScreens/mostActive/most_active_stocks.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:stocks_news_new/modals/most_active_stocks_res.dart';
+import 'package:stocks_news_new/providers/filter_provider.dart';
 import 'package:stocks_news_new/providers/most_active_provider.dart';
 import 'package:stocks_news_new/screens/drawerScreens/mostActive/item.dart';
+import 'package:stocks_news_new/screens/drawerScreens/widget/filter_ui_values.dart';
+import 'package:stocks_news_new/screens/drawerScreens/widget/market_data_filter.dart';
+import 'package:stocks_news_new/utils/bottom_sheets.dart';
 import 'package:stocks_news_new/utils/colors.dart';
 import 'package:stocks_news_new/widgets/html_title.dart';
 
@@ -23,11 +27,28 @@ class _MostActiveStocksState extends State<MostActiveStocks> {
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      if (context.read<MostActiveProvider>().data != null) {
+      MostActiveProvider provider = context.read<MostActiveProvider>();
+      if (provider.data != null) {
         return;
       }
-      context.read<MostActiveProvider>().getMostActiveData(type: 1);
+      provider.resetFilter();
+      provider.getData();
     });
+  }
+
+  void _onFilterClick() async {
+    FilterProvider provider = context.read<FilterProvider>();
+    if (provider.data == null) {
+      await context.read<FilterProvider>().getFilterData();
+    }
+    BaseBottomSheets().gradientBottomSheet(
+      title: "Filter Stock Screener",
+      child: MarketDataFilterBottomSheet(onFiltered: _onFiltered),
+    );
+  }
+
+  void _onFiltered(FilteredParams? params) {
+    context.read<MostActiveProvider>().applyFilter(params);
   }
 
   @override
@@ -35,48 +56,59 @@ class _MostActiveStocksState extends State<MostActiveStocks> {
     MostActiveProvider provider = context.watch<MostActiveProvider>();
     List<MostActiveStocksRes>? data = provider.data;
 
-    return BaseUiContainer(
-      error: provider.error,
-      hasData: data != null && data.isNotEmpty,
-      isLoading: provider.isLoading,
-      errorDispCommon: true,
-      showPreparingText: true,
-      onRefresh: () => provider.getMostActiveData(type: 1),
-      child: RefreshControl(
-        onRefresh: () async => provider.getMostActiveData(type: 1),
-        canLoadMore: provider.canLoadMore,
-        onLoadMore: () async =>
-            provider.getMostActiveData(loadMore: true, type: 1),
-        child: ListView.separated(
-          padding: EdgeInsets.only(
-            bottom: Dimen.padding.sp,
-            top: Dimen.padding.sp,
-          ),
-          itemBuilder: (context, index) {
-            if (data == null || data.isEmpty) {
-              return const SizedBox();
-            }
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (index == 0) HtmlTitle(subTitle: provider.extraUp?.subTitle),
-                MostActiveItem(
-                  data: data[index],
-                  index: index,
-                ),
-              ],
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return Divider(
-              color: ThemeColors.greyBorder,
-              height: 20.sp,
-            );
-          },
-          // itemCount: up?.length ?? 0,
-          itemCount: data?.length ?? 0,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        HtmlTitle(
+          subTitle: provider.extraUp?.subTitle ?? "",
+          onFilterClick: _onFilterClick,
+          margin: const EdgeInsets.only(top: 10, bottom: 10),
         ),
-      ),
+        if (provider.filterParams != null)
+          FilterUiValues(
+            params: provider.filterParams,
+            onDeleteExchange: (exchange) {
+              provider.exchangeFilter(exchange);
+            },
+          ),
+        BaseUiContainer(
+          error: provider.error,
+          hasData: data != null && data.isNotEmpty,
+          isLoading: provider.isLoading,
+          errorDispCommon: true,
+          showPreparingText: true,
+          onRefresh: () => provider.getData(type: 1),
+          child: RefreshControl(
+            onRefresh: () async => provider.getData(type: 1),
+            canLoadMore: provider.canLoadMore,
+            onLoadMore: () async => provider.getData(loadMore: true, type: 1),
+            child: ListView.separated(
+              padding: EdgeInsets.only(
+                bottom: Dimen.padding.sp,
+                top: Dimen.padding.sp,
+              ),
+              itemBuilder: (context, index) {
+                if (data == null || data.isEmpty) {
+                  return const SizedBox();
+                }
+                return const SizedBox();
+
+                // return MostActiveItem(
+                //   data: data[index],
+                //   index: index,
+                // );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider(
+                  color: ThemeColors.greyBorder,
+                  height: 20.sp,
+                );
+              },
+              itemCount: data?.length ?? 0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
