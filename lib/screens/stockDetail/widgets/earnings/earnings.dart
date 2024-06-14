f6d6ab5c793bcf8def1d20b893300fe7b25bacce
Nirmal <testnetleon@gmail.com>
2024-06-14 20:04:05 +0530

    detail pagessssss

--- lib/screens/stockDetail/widgets/earnings/earnings.dart
+++ lib/screens/stockDetail/widgets/earnings/earnings.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -5,7 +6,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:stocks_news_new/modals/faqs_res.dart';
 import 'package:stocks_news_new/modals/stockDetailRes/earnings.dart';
-import 'package:stocks_news_new/modals/stock_details_res.dart';
 import 'package:stocks_news_new/providers/stock_detail_new.dart';
 import 'package:stocks_news_new/screens/stockDetail/widgets/common_heading.dart';
 import 'package:stocks_news_new/screens/stockDetail/widgets/earnings/earning_history_item.dart';
@@ -14,10 +14,12 @@ import 'package:stocks_news_new/screens/stockDetail/widgets/sd_faq.dart';
 import 'package:stocks_news_new/screens/stockDetail/widgets/sd_top.dart';
 import 'package:stocks_news_new/utils/colors.dart';
 import 'package:stocks_news_new/utils/constants.dart';
+import 'package:stocks_news_new/utils/theme.dart';
 import 'package:stocks_news_new/widgets/base_ui_container.dart';
 import 'package:stocks_news_new/widgets/custom/refresh_indicator.dart';
 import 'package:stocks_news_new/widgets/custom_gridview.dart';
 import 'package:stocks_news_new/widgets/screen_title.dart';
+import 'package:stocks_news_new/widgets/spacer_horizontal.dart';
 import 'package:stocks_news_new/widgets/spacer_vertical.dart';
 
 class SdEarnings extends StatefulWidget {
@@ -57,7 +59,6 @@ class _SdEarningsState extends State<SdEarnings> {
   @override
   Widget build(BuildContext context) {
     StockDetailProviderNew provider = context.watch<StockDetailProviderNew>();
-    KeyStats? keyStats = provider.tabRes?.keyStats;
 
     return BaseUiContainer(
       isFull: true,
@@ -111,17 +112,58 @@ class _SdEarningsState extends State<SdEarnings> {
                           EpsEstimate? data =
                               provider.earnings?.epsEstimates?[index];
 
-                          return EpsEstimatesItem(
-                            data: data,
-                            isOpen: provider.openIndex == index,
-                            onTap: () {
-                              provider.setOpenIndex(
-                                provider.openIndex == index ? -1 : index,
-                              );
-                              provider.setOpenIndexEarningHistory(
-                                -1,
-                              );
-                            },
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              if (index == 0)
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        AutoSizeText(
+                                          maxLines: 1,
+                                          "Quarter",
+                                          style: stylePTSansRegular(
+                                            fontSize: 12,
+                                            color: ThemeColors.greyText,
+                                          ),
+                                        ),
+                                        const SpacerHorizontal(width: 10),
+                                        AutoSizeText(
+                                          maxLines: 1,
+                                          "Number of Estimates",
+                                          style: stylePTSansRegular(
+                                            fontSize: 12,
+                                            color: ThemeColors.greyText,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Divider(
+                                      color: ThemeColors.greyBorder,
+                                      height: 20.sp,
+                                      thickness: 1,
+                                    )
+                                  ],
+                                ),
+                              EpsEstimatesItem(
+                                data: data,
+                                isOpen: provider.openIndex == index,
+                                onTap: () {
+                                  provider.setOpenIndex(
+                                    provider.openIndex == index ? -1 : index,
+                                  );
+                                  provider.setOpenIndexEarningHistory(
+                                    -1,
+                                  );
+                                },
+                              ),
+                            ],
                           );
                         },
                         separatorBuilder: (BuildContext context, int index) {
@@ -144,7 +186,7 @@ class _SdEarningsState extends State<SdEarnings> {
                     children: [
                       ScreenTitle(
                         title:
-                            "${provider.tabRes?.keyStats?.name} Analyst EPS Estimates",
+                            "${provider.tabRes?.keyStats?.name} Earnings History by Quarter",
                       ),
                       ListView.separated(
                           padding: const EdgeInsets.only(top: 0, bottom: 20),
@@ -154,19 +196,61 @@ class _SdEarningsState extends State<SdEarnings> {
                             EarningHistory? data =
                                 provider.earnings?.earningHistory?[index];
 
-                            return EarningHistoryItem(
-                              data: data,
-                              isOpen: provider.openIndexEarningHistory == index,
-                              onTap: () {
-                                provider.setOpenIndexEarningHistory(
-                                  provider.openIndexEarningHistory == index
-                                      ? -1
-                                      : index,
-                                );
-                                provider.setOpenIndex(
-                                  -1,
-                                );
-                              },
+                            return Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                if (index == 0)
+                                  Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          AutoSizeText(
+                                            maxLines: 1,
+                                            "Quarter",
+                                            style: stylePTSansRegular(
+                                              fontSize: 12,
+                                              color: ThemeColors.greyText,
+                                            ),
+                                          ),
+                                          const SpacerHorizontal(width: 10),
+                                          AutoSizeText(
+                                            maxLines: 1,
+                                            "Reported EPS",
+                                            style: stylePTSansRegular(
+                                              fontSize: 12,
+                                              color: ThemeColors.greyText,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Divider(
+                                        color: ThemeColors.greyBorder,
+                                        height: 20.sp,
+                                        thickness: 1,
+                                      )
+                                    ],
+                                  ),
+                                EarningHistoryItem(
+                                  data: data,
+                                  isOpen:
+                                      provider.openIndexEarningHistory == index,
+                                  onTap: () {
+                                    provider.setOpenIndexEarningHistory(
+                                      provider.openIndexEarningHistory == index
+                                          ? -1
+                                          : index,
+                                    );
+                                    provider.setOpenIndex(
+                                      -1,
+                                    );
+                                  },
+                                ),
+                              ],
                             );
                           },
                           separatorBuilder: (BuildContext context, int index) {
