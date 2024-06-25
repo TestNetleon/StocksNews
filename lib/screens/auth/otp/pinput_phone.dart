00e226c356b7da74b18274c1a8bf80df2ac6e77c
Chetan <testnetleon@gmail.com>
2024-06-25 20:30:36 +0530

    Firebase Auth

---
+++ lib/screens/auth/otp/pinput_phone.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pinput/pinput.dart';
+import 'package:stocks_news_new/widgets/spacer_horizontal.dart';
+
+import '../../../utils/colors.dart';
+import '../../../utils/theme.dart';
+
+class CommonPinputPhone extends StatelessWidget {
+  final TextEditingController controller;
+  final FocusNode? focusNode;
+  final void Function(String)? onCompleted;
+  const CommonPinputPhone({
+    super.key,
+    required this.controller,
+    this.onCompleted,
+    this.focusNode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Pinput(
+        focusNode: focusNode,
+        length: 6,
+        controller: controller,
+        closeKeyboardWhenCompleted: true,
+        cursor: Text(
+          "|",
+          style: stylePTSansRegular(color: ThemeColors.white, fontSize: 30),
+        ),
+        showCursor: true,
+        separatorBuilder: (index) {
+          return const SpacerHorizontal(width: 5);
+        },
+        mouseCursor: SystemMouseCursors.basic,
+        defaultPinTheme: PinTheme(
+          width: 56.sp,
+          height: 65.sp,
+          textStyle: stylePTSansBold(color: ThemeColors.white, fontSize: 30),
+          decoration: BoxDecoration(
+            color: ThemeColors.transparent,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: Colors.grey),
+          ),
+        ),
+        focusedPinTheme: PinTheme(
+          width: 56.sp,
+          height: 70.sp,
+          textStyle: stylePTSansBold(color: ThemeColors.white, fontSize: 30),
+          decoration: BoxDecoration(
+            // color: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: ThemeColors.accent),
+          ),
+        ),
+        submittedPinTheme: PinTheme(
+          width: 56.sp,
+          height: 70.sp,
+          textStyle: stylePTSansBold(color: ThemeColors.white, fontSize: 30),
+          decoration: BoxDecoration(
+            // color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(color: ThemeColors.accent),
+          ),
+        ),
+        onCompleted: onCompleted,
+      ),
+    );
+  }
+}
