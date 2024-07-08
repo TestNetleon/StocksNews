2c9b6a3f3544f022119743951e2366d65680c2aa
ADMIN <testnetleon@gmail.com>
2024-07-08 13:00:45 +0530

    Update Lock in Screen

--- lib/screens/marketData/highsLowsBetaStocks/index.dart
+++ lib/screens/marketData/highsLowsBetaStocks/index.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:stocks_news_new/providers/home_provider.dart';
+import 'package:stocks_news_new/providers/user_provider.dart';
 import 'package:stocks_news_new/screens/marketData/highsLowsBetaStocks/high_beta_stocks.dart';
 import 'package:stocks_news_new/screens/marketData/highsLowsBetaStocks/low_beta_stocks.dart';
 import 'package:stocks_news_new/screens/marketData/highsLowsBetaStocks/negative_beta_stocks.dart';
+import 'package:stocks_news_new/screens/marketData/lock/common_lock.dart';
 import 'package:stocks_news_new/screens/tabs/home/widgets/app_bar_home.dart';
 import 'package:stocks_news_new/utils/colors.dart';
+import 'package:stocks_news_new/utils/utils.dart';
 import 'package:stocks_news_new/widgets/base_container.dart';
 
 import '../../../widgets/custom_tab_container.dart';
@@ -14,20 +19,56 @@ class HighLowsBetaStocksIndex extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BaseContainer(
+    UserProvider userProvider = context.watch<UserProvider>();
+    HomeProvider homeProvider = context.watch<HomeProvider>();
+
+    bool purchased = userProvider.user?.membership?.purchased == 1;
+
+    bool isLocked = false;
+
+    if (purchased) {
+      bool havePermissions = userProvider.user?.membership?.permissions?.any(
+              (element) =>
+                  element == "gap-up-stocks" || element == "gap-down-stocks") ??
+          false;
+      isLocked = !havePermissions;
+    } else {
+      if (!isLocked) {
+        isLocked = homeProvider.extra?.membership?.permissions?.any((element) =>
+                element == "gap-up-stocks" || element == "gap-down-stocks") ??
+            false;
+      }
+    }
+
+    Utils().showLog("GAP UP DOWN OPEN? $isLocked");
+
+    return BaseContainer(
       bottomSafeAreaColor: ThemeColors.background,
-      appBar: AppBarHome(
+      appBar: const AppBarHome(
         isPopback: true,
         canSearch: true,
       ),
-      body: CommonTabContainer(
-        scrollable: true,
-        // tabsPadding: EdgeInsets.only(bottom: 10.sp),
-        tabs: ["High Beta Stocks", "Low Beta Stocks", "Negative Beta Stocks"],
-        widgets: [
-          HighBetaStocks(),
-          LowsBetaStocks(),
-          NegativeBetaStocks(),
+      body: Stack(
+        children: [
+          const CommonTabContainer(
+            scrollable: true,
+            // tabsPadding: EdgeInsets.only(bottom: 10.sp),
+            tabs: [
+              "High Beta Stocks",
+              "Low Beta Stocks",
+              "Negative Beta Stocks"
+            ],
+            widgets: [
+              HighBetaStocks(),
+              LowsBetaStocks(),
+              NegativeBetaStocks(),
+            ],
+          ),
+          if (isLocked)
+            CommonLock(
+              isLocked: isLocked,
+              showLogin: true,
+            ),
         ],
       ),
     );
--- lib/screens/marketData/lowPriceStocks/index.dart
+++ lib/screens/marketData/lowPriceStocks/index.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:stocks_news_new/providers/home_provider.dart';
 import 'package:stocks_news_new/providers/low_prices_stocks.dart';
+import 'package:stocks_news_new/providers/user_provider.dart';
+import 'package:stocks_news_new/screens/marketData/lock/common_lock.dart';
 import 'package:stocks_news_new/screens/marketData/lowPriceStocks/low_price_stock_list.dart';
 import 'package:stocks_news_new/screens/tabs/home/widgets/app_bar_home.dart';
 import 'package:stocks_news_new/utils/colors.dart';
+import 'package:stocks_news_new/utils/utils.dart';
 import 'package:stocks_news_new/widgets/base_container.dart';
 import 'package:stocks_news_new/widgets/custom_tab_container.dart';
 import 'package:stocks_news_new/widgets/error_display_common.dart';
@@ -58,20 +62,52 @@ class LowPriceStocksData extends StatelessWidget {
     LowPriceStocksProvider provider = context.watch<LowPriceStocksProvider>();
     List<LowPriceStocksTabRes>? tabs = provider.tabs;
 
-    return CommonTabContainer(
-      onChange: (index) {
-        provider.resetFilter();
-        provider.tabChange(index);
-      },
-      scrollable: true,
-      tabs: List.generate(
-        tabs?.length ?? 0,
-        (index) => "${tabs?[index].key}",
-      ),
-      widgets: List.generate(
-        tabs?.length ?? 0,
-        (index) => LowPriceStocksList(index: index),
-      ),
+    UserProvider userProvider = context.watch<UserProvider>();
+    HomeProvider homeProvider = context.watch<HomeProvider>();
+
+    bool purchased = userProvider.user?.membership?.purchased == 1;
+
+    bool isLocked = false;
+
+    if (purchased) {
+      bool havePermissions = userProvider.user?.membership?.permissions?.any(
+              (element) =>
+                  element == "gap-up-stocks" || element == "gap-down-stocks") ??
+          false;
+      isLocked = !havePermissions;
+    } else {
+      if (!isLocked) {
+        isLocked = homeProvider.extra?.membership?.permissions?.any((element) =>
+                element == "gap-up-stocks" || element == "gap-down-stocks") ??
+            false;
+      }
+    }
+
+    Utils().showLog("GAP UP DOWN OPEN? $isLocked");
+
+    return Stack(
+      children: [
+        CommonTabContainer(
+          onChange: (index) {
+            provider.resetFilter();
+            provider.tabChange(index);
+          },
+          scrollable: true,
+          tabs: List.generate(
+            tabs?.length ?? 0,
+            (index) => "${tabs?[index].key}",
+          ),
+          widgets: List.generate(
+            tabs?.length ?? 0,
+            (index) => LowPriceStocksList(index: index),
+          ),
+        ),
+        if (isLocked)
+          CommonLock(
+            isLocked: isLocked,
+            showLogin: true,
+          ),
+      ],
     );
   }
 }
