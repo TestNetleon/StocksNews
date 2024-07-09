0ec92de9f1cd52a7e6cfabdb8f6196250c5d6f33
Chetan <testnetleon@gmail.com>
2024-07-09 11:05:36 +0530

    login

--- lib/screens/auth/refer/refer_code.dart
+++ lib/screens/auth/refer/refer_code.dart
@@ -66,7 +66,7 @@ class _ReferLoginState extends State<ReferLogin> {
   TextEditingController mobile = TextEditingController(text: "");
   TextEditingController name = TextEditingController(text: "");
   TextEditingController displayName = TextEditingController(text: "");
-  int affiliateStatus = 0;
+  bool affiliateStatus = false;
   bool numberVerified = true;
 
   String appSignature = "";
