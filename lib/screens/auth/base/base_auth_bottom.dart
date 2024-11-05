e5143fa717c70751e87f76774f6759a3333cfa77
Netleon <netleonweb@gmail.com>
2024-11-05 13:00:20 +0530

    changes

--- lib/screens/auth/base/base_auth_bottom.dart
+++ lib/screens/auth/base/base_auth_bottom.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -54,20 +55,20 @@ class _BaseAuthState extends State<BaseAuth> {
   }
 
   Future<void> _verifyPhoneNumber() async {
-    // if (kDebugMode) {
-    //   String mobile = phone.text;
-    //   Navigator.push(
-    //     context,
-    //     MaterialPageRoute(
-    //       builder: (context) => BaseVerifyOTP(
-    //         countryCode: countryCode ?? '+1',
-    //         phone: mobile,
-    //         verificationId: _verificationId ?? '',
-    //       ),
-    //     ),
-    //   );
-    //   return;
-    // }
+    if (kDebugMode) {
+      String mobile = phone.text;
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => BaseVerifyOTP(
+            countryCode: countryCode ?? '+1',
+            phone: mobile,
+            verificationId: _verificationId ?? '',
+          ),
+        ),
+      );
+      return;
+    }
     statusIsLoading(true);
 
     try {
