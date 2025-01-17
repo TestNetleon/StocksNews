2116ae200e7bf7215a68a2a8de18e295121ed4b9
Utkarsh <computer931999@gmail.com>
2025-01-17 20:04:56 +0530

    changes

--- lib/screens/tabs/home/home_container.dart
+++ lib/screens/tabs/home/home_container.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
@@ -21,7 +20,6 @@ import 'package:stocks_news_new/widgets/spacer_vertical.dart';
 import 'package:stocks_news_new/widgets/theme_button_small.dart';
 // import 'package:upgrader/upgrader.dart';
 import '../../../modals/home_insider_res.dart';
-import '../../../stocksScanner/screens/stockScanner/index.dart';
 import '../../../utils/colors.dart';
 import '../../../utils/utils.dart';
 import '../../../widgets/custom/refer.dart';
@@ -63,18 +61,18 @@ class _HomeContainerState extends State<HomeContainer> {
     }
     return Column(
       children: [
-        Visibility(
-          visible: provider.extra?.showTradingSimulator == true || kDebugMode,
-          child: ThemeButtonSmall(
-            text: 'Stock Scanner',
-            onPressed: () async {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => StocksScanner()),
-              );
-            },
-          ),
-        ),
+        // Visibility(
+        //   visible: provider.extra?.showTradingSimulator == true || kDebugMode,
+        //   child: ThemeButtonSmall(
+        //     text: 'Stock Scanner',
+        //     onPressed: () async {
+        //       Navigator.push(
+        //         context,
+        //         MaterialPageRoute(builder: (_) => StocksScanner()),
+        //       );
+        //     },
+        //   ),
+        // ),
         Visibility(
           visible: provider.extra?.phoneCodeError != null &&
               provider.extra?.phoneCodeError != '',
