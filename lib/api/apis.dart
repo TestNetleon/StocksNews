26f5e32acc46ef94ac1d38f777169a7a70af2b4c
Utkarsh <computer931999@gmail.com>
2025-03-11 11:06:57 +0530

    changes

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -10,11 +10,11 @@ class Apis {
   static const baseUrlLocal = "https://notification.stocks.news/api/v1";
 
   // With Debug condition
-  static const baseUrl = kDebugMode
-      ? "https://dev.stocks.news/api/v2"
-      : "https://app.stocks.news/api/v2";
+  // static const baseUrl = kDebugMode
+  //     ? "https://dev.stocks.news/api/v2"
+  //     : "https://app.stocks.news/api/v2";
 
-  // static const baseUrl = "https://app.stocks.news/api/v2";
+  static const baseUrl = "https://app.stocks.news/api/v2";
 
   // MARK: New UI APIs
   static const onBoarding = '/onboarding';
