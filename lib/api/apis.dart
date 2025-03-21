92cbff3722908964730dccbe8b7c838ad285afac
ADMIN <testnetleon@gmail.com>
2025-03-21 10:50:07 +0530

    lock and league

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -7,9 +7,9 @@ class Apis {
   static const baseUrlLocal = "https://notification.stocks.news/api/v1";
 
   // With Debug condition
-  static const baseUrl = kDebugMode
-      ? "https://dev.stocks.news/api/v2"
-      : "https://app.stocks.news/api/v2";
+  // static const baseUrl = kDebugMode
+  //     ? "https://dev.stocks.news/api/v2"
+  //     : "https://app.stocks.news/api/v2";
 
   // Main live server
    static const baseUrl = "https://app.stocks.news/api/v2";
